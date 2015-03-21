2c4330501e0733beb5f09f9cc10a52d9c4bf1756
danielle kefford <danielle_kefford@hotmail.com>
2015-03-21 17:10:26 -0400

    Finished chapter 5 save a few tricky exercises.

--- chapter5/src/Chapter5.purs
+++ chapter5/src/Chapter5.purs
@@ -1,5 +1,9 @@
 module Chapter5 where
 
+import Data.Array (map)
+import Data.Maybe
+import Data.Picture
+
 -- First set of exercises
 
 factorial :: Number -> Number
@@ -38,6 +42,28 @@ type Person = { first :: String,
 getCity :: Person -> String
 getCity { address = { city = c } } = c
 
---flatten :: forall a. [[a]] -> [a]
---flatten [] = []
---flatten (x : xs) = x ++ (flatten xs)
+flatten :: forall a. [[a]] -> [a]
+flatten [] = []
+flatten (x : xs) = x ++ (flatten xs)
+
+-- Fourth set of exercises
+
+origin = Point {x: 0, y: 0}
+circle = Circle origin 10
+
+transformAll :: [Shape] -> [Shape]
+transformAll = map transform where
+  transform (Circle _ r) = Circle origin (2*r)
+  transform (Rectangle _ w h) = Rectangle origin (2*w) (2*h)
+  transform s = s
+
+getText :: Shape -> Maybe String
+getText (Text _ s) = Just s
+getText _ = Nothing
+
+-- Fourth set of exercises
+
+area :: Shape -> Number
+area (Circle _ r) = Math.pi * r * r
+area (Rectangle _ w h) = w * h
+area _ = 0
